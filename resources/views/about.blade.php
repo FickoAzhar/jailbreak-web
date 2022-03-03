9c368c3f804041036ff18da5497fed6ef49ac90a
Majid Gigih Setiawan <88650135+themajidgigih@users.noreply.github.com>
2022-03-03 16:58:42 +0700

    memodifikasi file halaman about
    menambahkan section faq dan partners

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -37,6 +37,87 @@
     </section>
     <!-- END ABOUT US -->
 
-    
+    <!-- FAQ -->
+    <section class="faq py-0">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 pb-3">
+                    <div class="section-title text-center">
+                        <h6 class="text-color">FAQ</h6>
+                        <h2>Frequently Asked Question</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Accordion Item #1
+                        </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</strong> Fugiat repellat facere soluta. Quibusdam sequi in debitis quos perferendis est commodi nostrum molestias aliquam odio, unde voluptate voluptates eius reiciendis. Corporis!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Accordion Item #2
+                        </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                <strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</strong> Fugiat repellat facere soluta. Quibusdam sequi in debitis quos perferendis est commodi nostrum molestias aliquam odio, unde voluptate voluptates eius reiciendis. Corporis!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Accordion Item #3
+                        </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</strong> Fugiat repellat facere soluta. Quibusdam sequi in debitis quos perferendis est commodi nostrum molestias aliquam odio, unde voluptate voluptates eius reiciendis. Corporis!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END FAQ -->
+
+    <!-- OUR PARTNERS -->
+    <section>
+        <div class="container">
+            <h6 class="text-center text-color">Meet Our Partners</h6>
+            <h2 class="text-center fw-bold">Our Awesome Partners</h2>
+            <div class="row row-cols-1 row-cols-lg-3 py-2 text-center">
+                <div class="feature col py-2">
+                    <img width="150" src="asset/img/partners/google.png" alt="">
+                </div>
+                <div class="feature col py-2">
+                    <img width="150" src="asset/img/partners/facebook.png" alt="">
+                </div>
+                <div class="feature col py-2">
+                    <img width="150" src="asset/img/partners/instagram.png" alt="">
+                </div>
+                <div class="feature col py-2">
+                    <img width="150" src="asset/img/partners/youtube.png" alt="">
+                </div>
+                <div class="feature col py-2">
+                    <img width="150" src="asset/img/partners/twitter.png" alt="">
+                </div>
+                <div class="feature col">
+                    <img width="150" src="asset/img/partners/discord.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END OUR PARTNERS -->
 
 @endsection
